275f60b10fc8533c7966a28f7d7cfc7a59554ab7
Koushik <programmer.koushik@gmail.com>
2023-10-25 10:01:17 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const port = process.env.PORT;
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // Define your CORS options
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable sending credentials like cookies
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(port);
 }
 bootstrap();
